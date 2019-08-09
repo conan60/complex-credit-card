38e5bca6a0462d2f468a8a30af9fd7eee38ea3a6
conan60 <malek.gorchene@gmail.com>
2019-08-09 11:51:45 +0100

    Updates

---
+++ precache-manifest.b89c4f2a1ec2007e5a3e391705c04cd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d5fe217aca178bdb14c783402478ce",
+    "url": "/complex-credit-card/index.html"
+  },
+  {
+    "revision": "369caf7c450d00863013",
+    "url": "/complex-credit-card/static/css/main.8e09568f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/complex-credit-card/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "369caf7c450d00863013",
+    "url": "/complex-credit-card/static/js/main.6d23e145.chunk.js"
+  },
+  {
+    "revision": "c6956e967ea5d888cb88",
+    "url": "/complex-credit-card/static/js/runtime~main.f54a7e53.js"
+  }
+]);
